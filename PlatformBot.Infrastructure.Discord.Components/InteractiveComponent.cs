94ed40483ec406953ac064ba405148de0a69e1c3
a.lukianov <a.lukianov@dex-it.ru>
2025-04-17 16:04:04 +0300

    feature/main fix: add fixed files

---
+++ PlatformBot.Infrastructure.Discord.Components/InteractiveComponent.cs
@@ -0,0 +1,11 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace PlatformBot.Infrastructure.Discord.Components;
+
+/// <summary>
+/// Хранение данных компонента.
+/// </summary>
+/// <param name="Id">Идентификатор.</param>
+/// <param name="Action">Действие компонента.</param>
+internal record InteractiveComponent(string Id, Func<DiscordClient, ComponentInteractionCreateEventArgs, Task> Action);
